d0b2541f8e9b46d77f3268b5913059340e0fa7c0
arstan1112 <arstan.omurzakov@gmail.com>
2020-01-15 14:26:32 +0600

    Added like, dislike buttons with stats to the blog

--- assets/js/likes.js
+++ assets/js/likes.js
@@ -1,19 +1,30 @@
 $(document).ready(function () {
     console.log('likes');
-    var initialImpressionType;
-    var newImpressionType;
+    const topicId = $('#topicId').val();
+    const likeButton = $('#likeButton');
+    const dislikeButton = $('#dislikeButton');
+    const likeCounter = $('#likeCounter');
+    const dislikeCounter = $('#dislikeCounter');
+    const impressionBar = $('#impressionBar');
+    const impression = $('#userImpressionId').val();
 
-    var topicId = $('#topicId').val();
-    var likeButton = $('#likeButton');
-    var dislikeButton = $('#dislikeButton');
-    var userImpressionId = 0;
-    var impression = $('#userImpressionId').val();
-    var likeCounter = $('.likeCounter');
-    var dislikeCounter = $('.dislikeCounter');
+    let initialImpressionType;
+    let newImpressionType;
+    let impressionRate;
 
+    let userImpressionId = 0;
     if (impression) {
         userImpressionId = impression;
     }
+
+    let likeCounterVal = +likeCounter.text();
+    let dislikeCounterVal = +dislikeCounter.text();
+    if (likeCounterVal!==0) {
+        impressionRate = likeCounterVal/(likeCounterVal+dislikeCounterVal)*100;
+    } else {
+        impressionRate = 0;
+    }
+    impressionBar.css('width', impressionRate+'%');
 
     if (likeButton.hasClass("active")) {
         initialImpressionType = 1;
@@ -24,6 +35,21 @@ $(document).ready(function () {
     }
     console.log('initialType'+initialImpressionType);
 
+    $.fn.impressionRateSet = function () {
+        let likeCounterValue = +likeCounter.text();
+        let dislikeCounterValue = +dislikeCounter.text();
+        if (likeCounterValue!==0 && dislikeCounterValue!==0) {
+            impressionRate = likeCounterValue/(likeCounterValue+dislikeCounterValue)*100;
+        } else if (likeCounterValue===0 && dislikeCounterValue>0) {
+            impressionRate = 0;
+        } else if (dislikeCounterValue===0 && likeCounterValue>0) {
+            impressionRate = 100;
+        } else if (likeCounterValue===0 && dislikeCounterValue===0) {
+            impressionRate = 0;
+        }
+        console.log('inside impression function');
+    };
+
     likeButton.on('click', function (e) {
         let likeCounterValue = +likeCounter.text();
 
@@ -31,12 +57,17 @@ $(document).ready(function () {
             dislikeButton.attr("disabled", false);
             newImpressionType = 0;
             likeCounter.text(likeCounterValue-1);
+            $.fn.impressionRateSet();
+            impressionBar.css('width', impressionRate+'%');
+
             console.log('like unchecked, new type = '+newImpressionType)
         } else {
             dislikeButton.attr("disabled", true);
             newImpressionType = 1;
             likeCounter.text(likeCounterValue+1);
-            console.log('counter added');
+            $.fn.impressionRateSet();
+            impressionBar.css('width', impressionRate+'%');
+
             console.log('like checked, new type = '+newImpressionType)
         }
     });
@@ -48,11 +79,18 @@ $(document).ready(function () {
             likeButton.attr("disabled", false);
             newImpressionType = 0;
             dislikeCounter.text(dislikeCounterValue-1);
+            console.log(impressionRate);
+            $.fn.impressionRateSet();
+            impressionBar.css('width', impressionRate+'%');
+
             console.log('dislike unchecked, new type = '+newImpressionType)
         } else {
             likeButton.attr("disabled", true);
             newImpressionType = 2;
             dislikeCounter.text(dislikeCounterValue+1);
+            $.fn.impressionRateSet();
+            impressionBar.css('width', impressionRate+'%');
+
             console.log('dislike checked, new type = '+newImpressionType)
         }
     });
